f4122c0ccdf5de5253f08b400c4d4e5158726c27
RJ Cantojos <you@example.com>
2022-06-16 20:56:56 +0800

    test

--- main.py
+++ main.py
@@ -2,21 +2,22 @@ import base64
 import io
 from PIL import Image
 # from pyimagesearch.motion_detection.singlemotiondetector import SingleMotionDetector
-from imutils.video import VideoStream
+# from imutils.video import VideoStream
 from flask import Response
-from flask import Flask, jsonify
+from flask import Flask
 from flask import render_template
 import threading
-import datetime
+# import datetime
 import imutils
-import time
+# import time
 import cv2
 from ai import GestureDetector
 from flask_socketio import SocketIO, emit
 from io import StringIO
 import numpy as np
+# import argparse
 
-import os
+# import os
 
 outputFrame = None
 lock = threading.Lock()
@@ -152,18 +153,4 @@ def video_feed():
 
 # python main.py --ip 0.0.0.0 --port 8080 <- to run this web
 if __name__ == '__main__':
-
-    # ap = argparse.ArgumentParser()
-    # ap.add_argument("-i", "--ip", type=str, required=True,
-    #     help="ip address of the device")
-    # ap.add_argument("-o", "--port", type=int, required=True,
-    #     help="ephemeral port number of the server (1024 to 65535)")
-    # ap.add_argument("-f", "--frame-count", type=int, default=32,
-    #     help="# of frames used to construct the background model")
-    # args = vars(ap.parse_args())
-    # start a thread that will perform motion detection
-    t = threading.Thread(target=detect_motion, args=(32,))
-    t.daemon = True
-    t.start()
-
-    app.run(threaded=True)
+    app.run()
