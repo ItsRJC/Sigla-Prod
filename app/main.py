6e775a793c27436b45ec02a7a7b235e433a5fb93
RJ Cantojos <you@example.com>
2022-06-16 18:51:10 +0800

    Test

--- app/main.py
+++ app/main.py
@@ -150,21 +150,20 @@ def video_feed():
     )
     
 # python main.py --ip 0.0.0.0 --port 8080 <- to run this web
-if __name__ == '__main__':
 
-    ap = argparse.ArgumentParser()
-    ap.add_argument("-i", "--ip", type=str, required=True,
-        help="ip address of the device")
-    ap.add_argument("-o", "--port", type=int, required=True,
-        help="ephemeral port number of the server (1024 to 65535)")
-    ap.add_argument("-f", "--frame-count", type=int, default=32,
-        help="# of frames used to construct the background model")
-    args = vars(ap.parse_args())
-    # start a thread that will perform motion detection
-    t = threading.Thread(target=detect_motion, args=(
-        args["frame_count"],))
-    t.daemon = True
-    t.start()
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--ip", type=str, required=True,
+    help="ip address of the device")
+ap.add_argument("-o", "--port", type=int, required=True,
+    help="ephemeral port number of the server (1024 to 65535)")
+ap.add_argument("-f", "--frame-count", type=int, default=32,
+    help="# of frames used to construct the background model")
+args = vars(ap.parse_args())
+# start a thread that will perform motion detection
+t = threading.Thread(target=detect_motion, args=(
+    args["frame_count"],))
+t.daemon = True
+t.start()
 
-    app.run(host='0.0.0.0', port='8080', debug=True,
-        threaded=True, use_reloader=False)
+# app.run(host='0.0.0.0', port='8080', debug=True,
+#     threaded=True, use_reloader=False)
